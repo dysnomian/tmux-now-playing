58ad88593817708bfa6ebd5646ff636b1d9013bb
Liss McCabe <liss@eristiccode.com>
2019-05-13 12:57:56 -0700

    :put_litter_in_its_place: Delete unused test directory

--- test/itunes_helpers_test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bats
-
-load "itunes_helpers"
-
-@test "getting itunes track info" {
-  run itunes_current_track_property
-  [ "$status" -eq 0 ]
-  [ "$output" = "foo: no such file 'nonexistent_filename'" ]
-}
